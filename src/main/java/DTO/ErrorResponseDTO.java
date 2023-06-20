f5f63e59d3e1087deb22d3b6a4782c5e13227a7b
AlessandroCiroldi-Quix <alessandro.ciroldi@quix.it>
2023-06-20 17:01:26 +0200

    Aggiornato ErrorResponse

--- src/main/java/DTO/ErrorResponseDTO.java
+++ src/main/java/DTO/ErrorResponseDTO.java
@@ -1,4 +1,26 @@
 package DTO;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+
+@Data
 public class ErrorResponseDTO {
+    private boolean error;
+    private String type;
+    private String msg;
+
+    public ErrorResponseDTO(String type, String msg) {
+        this.error = true;
+        this.type = type;
+        this.msg = msg;
+    }
+
+    public static String getError(String type, String msg) {
+        try{
+            return new ObjectMapper().writeValueAsString(new ErrorResponseDTO(type, msg));
+        } catch (JsonProcessingException e) {
+            return "{error:true}";
+        }
+    }
 }
