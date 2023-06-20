e03fc800806433a72ff80c6781c5576667f73e38
AlessandroCiroldi-Quix <alessandro.ciroldi@quix.it>
2023-06-20 15:48:01 +0200

    Aggiornato jdbiProducer

--- src/main/java/Producer/jdbiProducer.java
+++ src/main/java/Producer/jdbiProducer.java
@@ -1,4 +1,15 @@
 package Producer;
+import org.jdbi.v3.core.Jdbi;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class jdbiProducer {
+    Jdbi jdbi;
+
+    jdbiProducer(){ jdbi = Jdbi.create("jdbc:mysql://localhost:3306/contestazioni","root",""); }
+    public Jdbi getJdbi(){
+        return jdbi;
+    }
+    /* Il costruttore inizializza un'istanza jdbi per creare la connessione al db
+     * il metodo pubblico 'getJdbi' rende l'istanza jdbi utilizzabile anche in altre classi */
 }
