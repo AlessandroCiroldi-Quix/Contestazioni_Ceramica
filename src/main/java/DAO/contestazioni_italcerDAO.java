7489885646a72ec5fbd845b5ddaf7a1764bfb19f
AlessandroCiroldi-Quix <alessandro.ciroldi@quix.it>
2023-06-21 15:07:48 +0200

    Aggiornato contestazioni_italcerDAO.java con la prima select di prova

--- src/main/java/DAO/contestazioni_italcerDAO.java
+++ src/main/java/DAO/contestazioni_italcerDAO.java
@@ -1,4 +1,29 @@
 package DAO;
 
+import DTO.contestazioni_italcerDTO;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import lombok.Data;
+import org.jdbi.v3.core.Jdbi;
+
+import java.util.List;
+
+@ApplicationScoped
+@Data
 public class contestazioni_italcerDAO {
+    @Inject
+    Producer.jdbiProducer jdbiProducer;
+
+    //NOME FUNZIONE DA CAMBIARE
+    public List<contestazioni_italcerDTO> getDato(){
+        Jdbi jdbi = jdbiProducer.getJdbi();
+
+        String query = "SELECT * FROM contestazioni_italcer";
+
+        return jdbi.withHandle(handle -> handle.createQuery(query)
+                .mapToBean(contestazioni_italcerDTO.class)
+                .list()
+        );
+    }
+
 }
