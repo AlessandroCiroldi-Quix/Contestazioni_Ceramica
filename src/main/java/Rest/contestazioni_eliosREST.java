b0ea38c537b05eb724ee59d618de8c8c48015da8
filippo.minelli <filippo.minelli@quix.it>
2023-06-26 15:32:55 +0200

    Elios completo e testato

--- src/main/java/Rest/contestazioni_eliosREST.java
+++ src/main/java/Rest/contestazioni_eliosREST.java
@@ -1,4 +1,143 @@
 package Rest;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.Data;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import DAO.*;
+import DTO.*;
+
+import java.util.List;
+
+
+@Path("elios")
+@Data
 public class contestazioni_eliosREST {
+    @Inject
+    contestazioni_eliosDAO contestazioni_eliosDAO;
+
+    @Path("/select")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(value = {
+
+            @APIResponse(
+                    responseCode = "500",
+                    description = "Errore",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Not found",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+
+            @APIResponse(
+                    responseCode = "200",
+                    description = "OK")}
+    )
+    public List<contestazioni_eliosDTO> GetContestazioni_elios() {
+        return contestazioni_eliosDAO.getData();
+    }
+
+    @Path("/add")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(value = {
+            @APIResponse(
+                    responseCode = "500",
+                    description = "Errore",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @APIResponse(
+                    responseCode = "409",
+                    description = "Conflict",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @APIResponse(
+                    responseCode = "200",
+                    description = "OK")}
+    )
+    //questo metodo controlla se esiste già la contestazione, se esiste lancia un errore,
+    //se non esiste lo aggiunge e poi fa una select dei dati aggiornati
+    public List<contestazioni_eliosDTO> addContestazione_elios(contestazioni_eliosDTO contestazione) {
+
+        if (!contestazioni_eliosDAO.findContestazione_elios(contestazione))
+            contestazioni_eliosDAO.addContestazione_elios(contestazione);
+        else
+            throw new WebApplicationException(
+                    Response.status(Response.Status.CONFLICT)
+                            .entity(ErrorResponse.getError("409", "Conflict"))
+                            .type(MediaType.APPLICATION_JSON)
+                            .build()
+            );
+        return contestazioni_eliosDAO.getData();
+    }
+
+    @Path("/delete")
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(value = {
+            @APIResponse(
+                    responseCode = "500",
+                    description = "Errore",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "not fnd",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @APIResponse(
+                    responseCode = "409",
+                    description = "Conflict",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @APIResponse(
+                    responseCode = "200",
+                    description = "OK")}
+    )
+    public List<contestazioni_eliosDTO> delContestazione_elios(contestazioni_eliosDTO contestazione) {
+
+        if (contestazioni_eliosDAO.findContestazione_elios(contestazione)) {
+            contestazioni_eliosDAO.deleteContestazione_elios(contestazione);
+        } else {
+            throw new WebApplicationException(
+                    Response.status(Response.Status.CONFLICT)
+                            .entity(ErrorResponse.getError("409", "Conflict"))
+                            .type(MediaType.APPLICATION_JSON)
+                            .build()
+            );
+        }
+        return contestazioni_eliosDAO.getData();
+    }
+
+    @Path("/update/{id}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(value = {
+            @APIResponse(
+                    responseCode = "500",
+                    description = "Errore",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Not found",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @APIResponse(
+                    responseCode = "409",
+                    description = "Conflict",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @APIResponse(
+                    responseCode = "200",
+                    description = "OK")}
+    )
+    public List<contestazioni_eliosDTO> updateContestazioni_elios(@PathParam("id") String id, contestazioni_eliosDTO contestazione) {
+        //id serve per identificare la contestazione che vogliamo aggiornare
+        contestazioni_eliosDAO.updateContestazioni_elios(contestazione, id);
+
+        return contestazioni_eliosDAO.getData();
+    }
 }
