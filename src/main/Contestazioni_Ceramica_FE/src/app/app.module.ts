679288486fd122db0733ad9d65913ea782d5f87f
filippo.minelli <filippo.minelli@quix.it>
2023-07-05 11:07:25 +0200

    Aggiunto componenti form e tabella

--- src/main/Contestazioni_Ceramica_FE/src/app/app.module.ts
+++ src/main/Contestazioni_Ceramica_FE/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormComponent } from './form/form.component';
+import { TabellaComponent } from './tabella/tabella.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    TabellaComponent
   ],
   imports: [
     BrowserModule,
