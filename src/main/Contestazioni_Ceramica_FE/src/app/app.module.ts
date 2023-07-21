22ad8e80fe81fa458bb04dc8b454866ea206236f
filippo.minelli <filippo.minelli@quix.it>
2023-07-21 11:39:07 +0200

    Elementi Frontend aggiunti + SCSS rules

--- src/main/Contestazioni_Ceramica_FE/src/app/app.module.ts
+++ src/main/Contestazioni_Ceramica_FE/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormComponent } from './form/form.component';
-import { TabellaComponents } from './tabella/tabella.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatTableModule} from '@angular/material/table';
@@ -22,9 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     MatDatepickerModule,
     MatTableModule,
-    TabellaComponents,
   ],
   providers: [contestazioniEliosservice],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent],  
 })
 export class AppModule {}
--- src/main/Contestazioni_Ceramica_FE/src/app/form/form.component.ts
+++ src/main/Contestazioni_Ceramica_FE/src/app/form/form.component.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Contestazioni_eliosDTO } from 'src/api/model/contesatzioni_eliosDTO';
-import { eliosFiltroDTO } from 'src/api/model/eliosFiltroDTO';
-import { contestazioniEliosservice } from 'src/api/service/contestazioni_elios.service';
+// Import delle librerie necessarie
+import { HttpClient } from '@angular/common/http'; // Per eseguire richieste HTTP
+import { Component, OnInit } from '@angular/core'; // Component decorator e OnInit interface
+import { Observable } from 'rxjs'; // Per supportare la programmazione reattiva con Observable
+import { Contestazioni_eliosDTO } from 'src/api/model/contesatzioni_eliosDTO'; // Import del modello per le contestazioni
+import { eliosFiltroDTO } from 'src/api/model/eliosFiltroDTO'; // Import del modello per il filtro
+import { contestazioniEliosservice } from 'src/api/service/contestazioni_elios.service'; // Import del servizio per le contestazioni
 
 @Component({
   selector: 'app-form',
@@ -12,33 +13,42 @@ import { contestazioniEliosservice } from 'src/api/service/contestazioni_elios.s
 })
 export class FormComponent implements OnInit {
   //* Variabili
-  valoreID_cont: string = ''; // Dichiarazione di una variabile di tipo stringa per l'ID della contestazione
-  valoreCod_art: string = '';
-  valoreCliente: string = '';
-  valoreDateIni: Date; // Dichiarazione della variabile per immagazzinare il valore del datepicker
-  valoreDateOut: Date;
-  valoreStato: string = '';
 
+  // Dichiarazione delle variabili per immagazzinare i valori del form
+  valoreID_cont: string = ''; // ID della contestazione
+  valoreCod_art: string = ''; // Codice articolo
+  valoreCliente: string = ''; // Nome del cliente
+  valoreDateIni: Date; // Data iniziale selezionata tramite datepicker
+  valoreDateOut: Date; // Data finale selezionata tramite datepicker
+  valoreStato: string = ''; // Stato selezionato tramite select
+
+  // Dichiarazione di un oggetto eliosFiltroDTO con valori di default per i filtri
   eliosFiltroDTO: eliosFiltroDTO = {
-  id: 0,
-  data_creazione: "",
-  data_ultima_mod: "",
-  cod_articolo: "",
-  rs_cliente: "",
-  stato: "",
-  reparto: "",
-  formato: "",
-  utente_creazione: ""}
-  datasource: Contestazioni_eliosDTO[] = []; // Dichiarazione di un array di tipo Contestazioni_eliosDTO per il datasource
+    id: 0,
+    data_creazione: '',
+    data_ultima_mod: '',
+    cod_articolo: '',
+    rs_cliente: '',
+    stato: '',
+    reparto: '',
+    formato: '',
+    utente_creazione: '',
+  };
+
+  // Dichiarazione di un array di tipo Contestazioni_eliosDTO per il datasource
+  datasource: Contestazioni_eliosDTO[] = [];
 
   //* ----------------------------------------------------------------
 
+  // Costruttore del componente con iniezione dei servizi
   constructor(
     private contestazioniService: contestazioniEliosservice,
     private http: HttpClient
   ) {}
 
+  // Metodo chiamato all'inizializzazione del componente
   ngOnInit(): void {
+    // Ottenimento delle contestazioni dal servizio all'avvio del componente
     this.contestazioniService.getContesatzioni_elios().subscribe((res) => {
       this.datasource = res;
       //console.log(JSON.stringify(res));
@@ -47,31 +57,37 @@ export class FormComponent implements OnInit {
 
   //! Ce ne deve essere una per ogni campo del form !
   //! Funzioni che permettono di immagazzinare il valore dei textForm in delle variabili
-  //? Metodo per prendere l'input di ID
+
+  // Metodo per prendere l'input di ID
   onInputID_cont(event: Event): string {
     this.valoreID_cont = (<HTMLInputElement>event.target).value;
     console.log('ID: ' + this.valoreID_cont); // Per verificare che effettivamente prende l'input
     return this.valoreID_cont;
   }
+
+  // Metodo per prendere l'input di Codice Articolo
   onInputCod_art(event: Event): string {
     this.valoreCod_art = (<HTMLInputElement>event.target).value;
     console.log('Cod_art: ' + this.valoreCod_art); //Queste console.log sono di prova
     return this.valoreCod_art;
   }
+
+  // Metodo per prendere l'input del Cliente
   onInputCliente(event: Event): string {
     this.valoreCliente = (<HTMLInputElement>event.target).value;
     console.log('Cliente: ' + this.valoreCliente); //Queste console.log sono di prova
     return this.valoreCliente;
   }
 
-  //? X i datepicker
-  // per salvare il valore della data
+  // Metodo per salvare il valore della data iniziale selezionata tramite il datepicker
   salvareValoreDateIni(event: Event): Date {
     const target = event.target as HTMLInputElement;
     this.valoreDateIni = new Date(target.value);
     console.log(this.valoreDateIni);
     return this.valoreDateIni;
   }
+
+  // Metodo per salvare il valore della data finale selezionata tramite il datepicker
   salvareValoreDateOut(event: Event): Date {
     const pick = event.target as HTMLInputElement;
     this.valoreDateOut = new Date(pick.value);
@@ -79,22 +95,29 @@ export class FormComponent implements OnInit {
     return this.valoreDateOut;
   }
 
-  //? Per i select
+  // Metodo per salvare lo stato selezionato tramite il select
   sceltaStato(event: Event): string {
     this.valoreStato = (<HTMLInputElement>event.target).value;
     console.log('Stato: ' + this.valoreStato); // Per verificare che effettivamente prende l'input
     return this.valoreStato;
   }
 
+  //* ----------------------------------------------------------------
   // Bottoni contestazioni
+
+  // Metodo chiamato quando il bottone "Modifica" viene cliccato
   bottoneModifica() {
     console.log('bottone modifica funzionante!');
   }
+
+  // Metodo chiamato quando il bottone "Visualizza" viene cliccato
   bottoneVisualizza() {
     console.log('bottone visualizza funzionante!');
   }
+
+  // Metodo chiamato quando il bottone "Elimina" viene cliccato
   bottoneElimina(id: number) {
-    const url = "http://localhost:8080/elios/delete/" + id;
+    const url = 'http://localhost:8080/elios/delete/' + id;
 
     fetch(url, {
       method: 'DELETE',
@@ -109,19 +132,23 @@ export class FormComponent implements OnInit {
       .catch((error) => {
         console.error('Si è verificato un errore nella richiesta:', error);
       });
-      location.reload();
+    location.reload();
   }
 
+  // Metodo chiamato quando il bottone "Scarica" viene cliccato
   bottoneScarica() {
     console.log('bottone scarica funzionante!');
   }
 
-  getContestationByFiltro(id:  any){
-    console.log("aPaolo", id)
+  // Metodo per ottenere le contestazioni tramite i filtri selezionati
+  getContestationByFiltro(id: any) {
+    console.log(id);
     this.eliosFiltroDTO.id = id;
-    this.contestazioniService.eliosFiltro(this.eliosFiltroDTO).subscribe((res) => {
-      console.log("andrà?",  JSON.stringify(res));
-      this.datasource = res
-    });
+    this.contestazioniService
+      .eliosFiltro(this.eliosFiltroDTO)
+      .subscribe((res) => {
+        console.log(JSON.stringify(res));
+        this.datasource = res;
+      });
   }
 }
